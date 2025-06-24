e8782750109f7faf7c9b31457ce7acfae0b0ad8f
JorgeHB14 <jorge.hernananz@live.com>
2025-06-24 10:45:54 +0200

    patata

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -9,9 +9,14 @@
 #' @examples
 #' x <- "alpha,beta"
 #' strsplit(x, ",")
-strsplit1 <- function(x, split) {
+str_split_one <- function(string, pattern, n=Inf) {
 
-  strsplit(x, split=split)[[1]]
+  stopifnot(is.character(string), length(string)<=1)
 
+  if (length(string)==1) {
+    stringr::str_split(string=string, pattern=pattern, n=n)[[1]]
+  } else {
+    character(0)
+  }
 }
 
